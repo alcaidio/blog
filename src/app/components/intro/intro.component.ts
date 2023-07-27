d2882beb014a23c85dc6ce8e4c2558742ee3f4ee
timothy alcaide <timothy@harvestr.io>
2023-07-27 21:23:25 +0200

    feat: home page design

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ContactComponent } from '../contact/contact.component';
 
 @Component({
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,6 @@ module.exports = {
       }
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
 }
 
